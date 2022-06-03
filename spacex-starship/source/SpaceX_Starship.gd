b15c66c6ba48b565a7e50af276eaa7da8c07e096
Difint <yarm-public@protonmail.com>
2022-06-03 21:44:34 +0700

    - Added code for thrusters

---
+++ spacex-starship/source/SpaceX_Starship.gd
@@ -0,0 +1,12 @@
+extends Spatial
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_SpacecraftControl_thrust(enabled):
+	$Exhause.emitting = enabled
